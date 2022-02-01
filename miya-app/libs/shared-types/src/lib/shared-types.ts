b58f3e59c9c7c74a632b2d62d2f34d31010f2c37
Wera23 <werachodanionek@gmail.com>
2022-02-01 12:23:53 +0100

    changes

--- miya-app/libs/shared-types/src/lib/shared-types.ts
+++ miya-app/libs/shared-types/src/lib/shared-types.ts
@@ -18,6 +18,23 @@ export class Retriever {
   long: number;
   @IsString() @IsOptional() readonly instagram: string;
   @IsString() @IsOptional() readonly facebook: string;
+  @IsString() @IsOptional() readonly image: string;
+}
+export class EditRetriever {
+  @IsString() readonly name: string | undefined;
+  @IsString() readonly age: string | undefined;
+  @IsString() readonly city: string | undefined;
+  @IsString() readonly voivodeship: string | undefined;
+  @IsString() readonly description: string | undefined;
+  @Type(() => Number)
+  @IsNumber()
+  lat: number | undefined;
+  @Type(() => Number)
+  @IsNumber()
+  long: number | undefined;
+  @IsString() @IsOptional() readonly instagram: string | undefined;
+  @IsString() @IsOptional() readonly facebook: string | undefined;
+  @IsString() @IsOptional() readonly image: string | undefined;
 }
 
 export class User {
@@ -26,6 +43,15 @@ export class User {
   @IsString() readonly userPassword: string;
   @IsString() @IsOptional() readonly dateOfBirth: string;
   @IsString() @IsOptional() readonly userDescription: string;
+  @IsString() @IsOptional() readonly userAddress: string;
+  @IsString() @IsOptional() readonly userImage: string;
+}
+
+export class EditUser {
+  @IsString() @IsOptional() readonly dateOfBirth: string | undefined;
+  @IsString() @IsOptional() readonly userDescription: string | undefined;
+  @IsString() @IsOptional() readonly userAddress: string | undefined;
+  @IsString() @IsOptional() readonly userImage: string | undefined;
 }
 
 export class UserLogin {
